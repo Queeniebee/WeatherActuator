fb4819b6da34a0930ac6f00b803bd69689a6ae75
Queeniebee <joasqueeniebee@gmail.com>
2014-11-03 18:58:24 -0500

    refactored code to make it more RESTful, cityname now being saved

--- nodejs/server.js
+++ nodejs/server.js
@@ -10,9 +10,10 @@ var exphbs  = require('express3-handlebars');
 var bodyParser = require('body-parser');
 var firmata = require('firmata');
 var serialPort = require('serialport');
+
 var ledPin = 13;
 var tempValue = 0;
-var cityname = "";
+// var cityname = "";
 var location = "";
 var port = "/dev/tty.usbmodemfd121";
 var board = new firmata.Board(port, function(err){
@@ -24,19 +25,18 @@ var board = new firmata.Board(port, function(err){
 	board.pinMode(ledPin, board.MODES.OUTPUT);
 
 });
-var app = express();
 
+var app = express();
 //use static local files
 app.use(express.static(__dirname + '/public'));
 //handlebars
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-
 //body parser
 app.use(bodyParser());
 
 //server setup
-var server = app.listen(3000, function(){
+var server = app.listen(3300, function(){
   console.log('Listening on port %d', server.address().port);
 });
 
@@ -50,20 +50,24 @@ function sendIndexPage(req, res){
 //ALMOST WORKS
 function getCity(req, res){
 	
-	res.render('layouts/city');
-}
+// 	res.render('layouts/city');
+	var cityName = req.param('cityname');
+	console.log(cityName);
+	getCityTemp(cityName);
 //function that gets the temp of the specified city
-//FAILS
-function getCityTemp(err, req, res){
+// function getCityTemp(err, req, res){
+function getCityTemp(err, cityName){
 
 //     if (!err && res.statusCode === 200) {
     if (!err) {
-	cityname = req.params[1];
+// 	cityname = req.body.cityname;
+// 	cityname = req.params[1];
+// 	cityname = data.cityname;
 	var query = "http://api.openweathermap.org/data/2.5/weather?";
   	var key = "";
   	var options = [];
   	options["APPID"] = "d9a5fc0bed270bb5e6e3c6ae3d0b2fe7";
-  	options["q"] = cityname;
+  	options["q"] = cityName; //cityName from above
 
 	for (key in options){
 		var str = key + '=' + options[key];
@@ -73,8 +77,8 @@ function getCityTemp(err, req, res){
 	  query = query.slice(0, -1);
         var data = JSON.parse(res);
         console.log("Response from API:");
-        //console.log(data);
-		
+        console.log(data);
+// 		cityname = data.name;
         //convert the temperature from Kelvin to Celsius
         data.main.temp = data.main.temp - 273.15;
         data.main.temp = data.main.temp.toFixed(1);
@@ -93,12 +97,13 @@ function getCityTemp(err, req, res){
 	res.render('layouts/city');
 //   request.get(query, getCityTemp);
 }
-}
+} //end getCityTemp function
+} //ends getCity function
 
 app.get('/', sendIndexPage);
 //this GETs the city and sets it on the physical device
-app.get('/cityname/:cityname/', getCity);
+app.get('/ask/:cityname/', getCity);
 
 
 //this GETs the actual temp from the OpenWeather API
-app.get('/ask', getCityTemp);
+// app.get('/ask', getCityTemp);
