ae616b66c3ba29c1cf8a41c4c00cfd73ac97edb3
Queeniebee <joasqueeniebee@gmail.com>
2014-11-04 10:33:56 -0500

    very basic restful example

--- nodejs/server.js
+++ nodejs/server.js
@@ -9,6 +9,7 @@ var express = require('express');
 var exphbs  = require('express3-handlebars');
 var bodyParser = require('body-parser');
 var request = require('request');
+var mongo = require('mongodb');
 
 var firmata = require('firmata');
 var serialPort = require('serialport');
@@ -16,7 +17,13 @@ var serialPort = require('serialport');
 var ledPin = 13;
 var tempValue = 0;
 
-var location = "";
+var Server = mongo.Server;
+var Db = mongo.Db;
+var BSON = mongo.BSONPure;
+
+var server = new Server('localhost', 3300, {auto_reconnect: true});
+db = new Db('waDB', server, {safe: true});
+
 
 var port = "/dev/tty.usbmodemfd121";
 var board = new firmata.Board(port, function(err){
@@ -43,16 +50,105 @@ var server = app.listen(3300, function(){
   console.log('Listening on port %d', server.address().port);
 });
 
+db.open(function(err, db) {
+    if(!err) {
+        console.log("Connected to 'waDB' database");
+        db.collection('devices', {safe:true}, function(err, collection) {
+            if (err) {
+                console.log("The 'wines' collection doesn't exist. Creating it with sample data...");
+                populateDB();
+            }
+        });
+    }
+});
+
+exports.findById = function(req, res) {
+    var id = req.params.id;
+    console.log('Retrieving wine: ' + id);
+    db.collection('devices', function(err, collection) {
+        collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, item) {
+            res.send(item);
+        });
+    });
+};
+
+exports.findAll = function(req, res) {
+    db.collection('devices', function(err, collection) {
+        collection.find().toArray(function(err, items) {
+            res.send(items);
+        });
+    });
+};
+
+exports.addDevice = function(req, res) {
+    var device = req.body;
+    console.log('Adding device: ' + JSON.stringify(wine));
+    db.collection('devices', function(err, collection) {
+        collection.insert(device, {safe:true}, function(err, result) {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                console.log('Success: ' + JSON.stringify(result[0]));
+                res.send(result[0]);
+            }
+        });
+    });
+}
+
+exports.updateDeviceLocation = function(req, res) {
+    var id = req.params.id;
+    var device = req.body;
+    delete device._id;
+    console.log('Updating wine: ' + id);
+    console.log(JSON.stringify(device));
+    db.collection('devices', function(err, collection) {
+        collection.update({'_id':new BSON.ObjectID(id)}, device, {safe:true}, function(err, result) {
+            if (err) {
+                console.log('Error updating wine: ' + err);
+                res.send({'error':'An error has occurred'});
+            } else {
+                console.log('' + result + ' document(s) updated');
+                res.send(device);
+            }
+        });
+    });
+}
+
+exports.deleteDevice = function(req, res) {
+    var id = req.params.name;
+    console.log('Deleting device: ' + id);
+    db.collection('devices', function(err, collection) {
+        collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
+            if (err) {
+                res.send({'error':'An error has occurred - ' + err});
+            } else {
+                console.log('' + result + ' document(s) deleted');
+                res.send(req.body);
+            }
+        });
+    });
+}
 //serve up index page from views/layouts
 function sendIndexPage(req, res){
 	res.render('layouts/top');
 }
 
+function setDeviceName(req, res){
+	var actuatorName = req.param('device');
+	console.log(actuatorName);
+
+	res.render('layouts/actuatorName', {actuator: actuatorName});
+
+}
+
 //function that sets the city for the Actuator
-//ALMOST WORKS
+//WORKS
 function getCity(req, res, body){
-	var actuatorName = req.param('name');
+	var actuatorName = req.param('device');
+	app.locals.actuator = actuatorName;
+
 	var cityName = req.param('cityname');
+	
 	var query = "http://api.openweathermap.org/data/2.5/weather?";
   	var key = "";
   	var options = [];
@@ -63,14 +159,12 @@ function getCity(req, res, body){
 		var str = key + '=' + options[key];
 		query = query + str + '&';
 	}
-	  //remove the last character '&' from the query
+	//remove the last character '&' from the query
 	query = query.slice(0, -1);
 	//checking if variable is being set
 	console.log(cityName);
-	console.log(actuatorName);
 	
-// 	var callback = getCityTemp();
-
+	
 		console.log("Query: " + query);
 	//function that gets the temp of the specified city
 		var callback = function getCityTemp(error, response, body){
@@ -84,7 +178,6 @@ function getCity(req, res, body){
         	data.main.temp = data.main.temp - 273.15;
         	data.main.temp = data.main.temp.toFixed(1);
         	console.log(data.main.temp);
-
 			tempValue = data.main.temp;
 			console.log(tempValue);
 /*
@@ -102,15 +195,12 @@ function getCity(req, res, body){
 	}
 
 		request.get(query, callback);
-// 		res.status(200).send(query);
 } 
-app.set('name', 'Weather Actuator');
 app.get('/', sendIndexPage);
-//this GETs the city and sets it on the physical device
-app.get('/:name/:cityname/', getCity);
+app.get('/:device', setDeviceName);
+app.get('/:device/:cityname', getCity);
+
+
 // app.get('/actuator/displaying', getDisplay);
 
-//this GETs the actual temp from the OpenWeather API
-// app.get('/ask', getCityTemp);
 
-// '/:name/:cityname'
